8f2bd7b2e8ca947430edfdfa762d1ca316ee0393
sukimixcr <sukimixcr@gmail.com>
2024-07-24 22:30:09 -0700

    demo working with many dropabbles

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,51 @@
 import React, { useState } from "react";
 import { DndContext } from "@dnd-kit/core";
-
 import { Droppable } from "./Droppable";
 import { Draggable } from "./Draggable";
 
 function App() {
-  const [isDropped, setIsDropped] = useState(false);
-  const draggableMarkup = <Draggable>Drag me</Draggable>;
+  // const [isDropped, setIsDropped] = useState(false);
+  // const draggableMarkup = <Draggable>Drag me</Draggable>;
+
+  const containers = ["A", "B", "C"];
+  const [parent, setParent] = useState(null);
+  const draggableMarkup = <Draggable id="draggable">Drag me</Draggable>;
+
+  // return (
+  //   <>
+  //     <DndContext onDragEnd={handleDragEnd}>
+  //       {!isDropped ? draggableMarkup : null}
+  //       <Droppable>{isDropped ? draggableMarkup : "Drop here"}</Droppable>
+  //     </DndContext>
+  //   </>
+  // );
 
   return (
     <DndContext onDragEnd={handleDragEnd}>
-      {!isDropped ? draggableMarkup : null}
-      <Droppable>{isDropped ? draggableMarkup : "Drop here"}</Droppable>
+      {parent === null ? draggableMarkup : null}
+
+      {containers.map((id) => (
+        // We updated the Droppable component so it would accept an `id`
+        // prop and pass it to `useDroppable`
+        <Droppable key={id} id={id}>
+          {parent === id ? draggableMarkup : "Drop here"}
+        </Droppable>
+      ))}
     </DndContext>
   );
 
+  // function handleDragEnd(event) {
+  //   if (event.over && event.over.id === "droppable") {
+  //     setIsDropped(true);
+  //   }
+  // }
+
   function handleDragEnd(event) {
-    if (event.over && event.over.id === "droppable") {
-      setIsDropped(true);
-    }
+    const { over } = event;
+
+    // If the item is dropped over a container, set it as the parent
+    // otherwise reset the parent to `null`
+    setParent(over ? over.id : null);
   }
 }
 export default App;
