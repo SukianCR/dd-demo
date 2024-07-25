08501e671d23062f23506f9ce455ac79b486c2f4
sukimixcr <sukimixcr@gmail.com>
2024-07-24 21:50:32 -0700

    first sample working

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { DndContext } from "@dnd-kit/core";
+
+import { Droppable } from "./Droppable";
+import { Draggable } from "./Draggable";
+
+function App() {
+  const [isDropped, setIsDropped] = useState(false);
+  const draggableMarkup = <Draggable>Drag me</Draggable>;
+
+  return (
+    <DndContext onDragEnd={handleDragEnd}>
+      {!isDropped ? draggableMarkup : null}
+      <Droppable>{isDropped ? draggableMarkup : "Drop here"}</Droppable>
+    </DndContext>
+  );
+
+  function handleDragEnd(event) {
+    if (event.over && event.over.id === "droppable") {
+      setIsDropped(true);
+    }
+  }
+}
+export default App;
